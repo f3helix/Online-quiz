bb201144e958d9cf5ae41bfc9f83838d5287a731
f3helix <karanfilovbogdan@gmail.com.com>
2024-07-05 19:33:26 +0300

    forms

--- quizApp/urls.py
+++ quizApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from .views import *
+from quizApp import views as quizApp
 
 urlpatterns = [
     path('', QuisListView.as_view(), name= 'quiz_list'),
-    # path('add/', CreateQuisView.as_view(), name= 'add'),
+    path('add_quiz/', CreateQuisView.as_view(), name= 'add_quiz'),
+    path('add_quiz', quizApp.create_quiz, name = 'add_quiz'),
+    # path('base', quizApp.home, name = 'base')
 ]
--- quizApp/views.py
+++ quizApp/views.py
@@ -8,43 +8,20 @@ from django.http  import HttpResponseRedirect
 
 
 def home(request):
-    return render(request, 'quizApp/base.html')
+    return render(request, 'base.html')
 
-
+ 
 
 
 def create_quiz(request):
-    if request.method == 'POST':
-        form = QuizForm(request.POST)
-        if form.is_valid():
-            quiz = form.save(commit=False)
-            quiz.created_by = request.user
-            quiz.save()
-            return redirect('home')
-    else:
-        form = QuizForm()
-    return render(request, 'quizApp/add_quiz.html', {'form': form})
-
-
-def edit_quiz(request, quiz_id):
-    quiz = get_object_or_404(Quiz, pk=quiz_id)
-    if request.method == 'POST':
-        form = QuizForm(request.POST, instance=quiz)
-        if form.is_valid():
-            form.save()
-            return redirect('base')
-    else:
-        form = QuizForm(instance=quiz)
-    return render(request, 'quizApp/edit_quiz.html', {'form': form, 'quiz': quiz})
-
-
-def delete_quiz(request, quiz_id):
-    quiz = get_object_or_404(Quiz, pk=quiz_id)
-    if request.method == 'POST':
-        quiz.delete()
-        return redirect('base')
-    return render(request, 'quizApp/delete_quiz.html', {'quiz': quiz})
-
+    if request.method == "POST":
+        print(request.POST)
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        quiz = Quiz(title=title, description=description, created_by=request.user)
+        quiz.save()
+        
+    return render (request, 'quizApp/add_quiz.html')
 
 
 class QuisListView(ListView):
@@ -53,17 +30,55 @@ class QuisListView(ListView):
     template_name = "quizApp/quiz_list.html"
 
    
+
+
+
+class CreateQuisView(CreateView):
+    model = Quiz
+    form_class = QuizForm
+    template_name = 'quizApp/add_quiz.html'
+    success_url = '/'
+
+
+
+    # if request.method == 'POST':
+    #     form = QuizForm(request.POST)
+    #     if form.is_valid():
+    #         quiz = form.save(commit=False)
+    #         quiz.created_by = request.user
+    #         quiz.save()
+    #         return redirect('home')
+    # else:
+    #     form = QuizForm()
+    # return render(request, 'quizApp/add_quiz.html', {'form': form})
+
+
+# def edit_quiz(request, quiz_id):
+#     quiz = get_object_or_404(Quiz, pk=quiz_id) 
+#     if request.method == 'POST':
+#         form = QuizForm(request.POST, instance=quiz)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('base')
+#     else:
+#         form = QuizForm(instance=quiz)
+#     return render(request, 'quizApp/edit_quiz.html', {'form': form, 'quiz': quiz})
+
+
+# def delete_quiz(request, quiz_id):
+#     quiz = get_object_or_404(Quiz, pk=quiz_id)
+#     if request.method == 'POST':
+#         quiz.delete()
+#         return redirect('base')
+#     return render(request, 'quizApp/delete_quiz.html', {'quiz': quiz})
+
+
+
+
 # class QuisDetailView(DetailView):
 #     model = Task
 #     context_object_name = "task"
 #     template_name = "tasktrack_ap/taskdetail.html"
-
-
-# class CreateQuisView(CreateView):
-#     model = Task
-#     form_class = TaskForm
-#     template_name = 'tasktrack_ap/create_task.html'
-#     success_url = '/'
 
 
 # class QuisUpdateView(UpdateView):
